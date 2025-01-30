0ce82176bb71f35a3cbab1b512b33037821a7f4d
Horagg42 <thoragg@t-online.de>
2025-01-30 22:57:10 +0100

    index.js

--- index.js
+++ index.js
@@ -29,5 +29,5 @@ app.get("/notion/lore", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`API läuft auf Port ${PORT}`));
+app.listen(PORT, () => console.log(`API läuft auf http://afterlife-crisis-api.onrender.com/notion/lore`));
 
